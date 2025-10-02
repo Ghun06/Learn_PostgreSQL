2acb91509e39ed5a9eb0cf0c5c12213391317133
TuanHung06 <95094186+Ghun06@users.noreply.github.com>
2025-10-02 13:14:06 +0700

    Update Second Highest Salary.sql

--- Function_TRIM_BLR_TRIM/Second Highest Salary.sql
+++ Function_TRIM_BLR_TRIM/Second Highest Salary.sql
@@ -7,3 +7,12 @@ FROM (
     FROM Employee
 ) t
 WHERE rnk = 2;
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+    SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT N, 1
+  );
+END
