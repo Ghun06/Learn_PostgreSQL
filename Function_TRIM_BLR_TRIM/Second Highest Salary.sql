bcddb90612a52e451136a69e775b61214b8372c6
TuanHung06 <95094186+Ghun06@users.noreply.github.com>
2026-04-03 13:19:32 +0700

    Add query to calculate tiv_2016 sum with conditions
    Added a new query to calculate rounded sum of tiv_2016 based on specific conditions.

--- Function_TRIM_BLR_TRIM/Second Highest Salary.sql
+++ Function_TRIM_BLR_TRIM/Second Highest Salary.sql
@@ -16,3 +16,13 @@ SET N=N-1;
     SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT N, 1
   );
 END
+-- add new query
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance 
+WHERE tiv_2015 IN (
+    SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(*) > 1
+)
+AND 
+(lat, lon) IN (
+    SELECT lat, lon FROM Insurance GROUP BY lat, lon HAVING COUNT(*) = 1
+)
